8d68b8c757d34f9fdc172a4c208415817fceed5e
zeroKilo <b_d_b@web.de>
2015-07-13 20:16:10 +0200

    initial commit
    initial commit

---
+++ BitDisplay/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BitDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(747, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Courier New", 8.25F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(600, 25);
+            this.toolStripTextBox1.Text = "324979413e003f4038403f001f2040201f00ffffffffffff";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(53, 22);
+            this.toolStripButton1.Text = "MakeBits";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Checked = true;
+            this.toolStripButton2.CheckOnClick = true;
+            this.toolStripButton2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(74, 22);
+            this.toolStripButton2.Text = "Flip Up/Down";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.White;
+            this.pb1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb1.Location = new System.Drawing.Point(0, 25);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(747, 101);
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 126);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bit Display";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ BitDisplay/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        private string CleanString(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            string list = "0123456789abcdef";
+            input = input.ToLower().Replace("0x","");
+            foreach (char c in input)
+                foreach (char c2 in list)
+                    if (c == c2)
+                        sb.Append(c);
+            return sb.ToString();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            DrawBits();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            DrawBits();
+        }
+
+        private void DrawBits()
+        {
+            try
+            {
+                string input = CleanString(toolStripTextBox1.Text);
+                toolStripTextBox1.Text = input;
+                byte[] buff = StringToByteArray(input);
+                Graphics g = pb1.CreateGraphics();
+                g.Clear(Color.White);
+                FontFamily fontFamily = new FontFamily("Courier New");
+                Font font = new Font(fontFamily, 10, FontStyle.Regular, GraphicsUnit.Pixel);
+                Brush solidBrush = new SolidBrush(Color.FromArgb(255, 0, 0, 255));
+                if (!toolStripButton2.Checked)
+                {
+                    for (int i = 0; i < 8; i++)
+                        g.DrawString((7 - i).ToString(), font, solidBrush, new PointF(10, i * 10 + 8));
+                    for (int i = 0; i < buff.Length; i++)
+                        for (int j = 0; j < 8; j++)
+                            if (((1 << j) & buff[i]) != 0)
+                                g.FillRectangle(Brushes.Black, new Rectangle(i * 10 + 20, (7 - j) * 10 + 10, 8, 8));
+                }
+                else
+                {
+                    for (int i = 0; i < 8; i++)
+                        g.DrawString(i.ToString(), font, solidBrush, new PointF(10, i * 10 + 8));
+                    for (int i = 0; i < buff.Length; i++)
+                        for (int j = 0; j < 8; j++)
+                            if (((0x80 >> j) & buff[i]) != 0)
+                                g.FillRectangle(Brushes.Black, new Rectangle(i * 10 + 20, (7 - j) * 10 + 10, 8, 8));
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
